c18161ebe8b7732ba648bc49b6f8360e37a536ac
Theodore Dubois <tbodt@google.com>
2025-03-06 22:07:05 -0800

    Update knausj_talon

--- talon/tbodt/tok.py
+++ talon/tbodt/tok.py
@@ -1,5 +1,4 @@
 from talon import Module, Context, grammar
-from ..knausj_talon.core.vocabulary.vocabulary import vocabulary as talon_vocabulary
 
 mod = Module()
 mod.mode('tok')
@@ -14,7 +13,7 @@ ww = {}
 for w in '''a akesi ala alasa ale anpa ante anu awen e en esun ijo ike ilo insa jaki jan jelo jo kala kalama kama kasi ken kepeken kili kiwen ko kon kule kulupu kute la lape laso lawa len lete li lili linja lipu loje lon luka lukin lupa ma mama mani meli mi mije moku moli monsi mu mun musi mute nanpa nasa nasin nena ni nimi noka o olin ona open pakala pali palisa pan pana pi pilin pimeja pini pipi poka poki pona pu sama seli selo seme sewi sijelo sike sin sina sinpin sitelen sona soweli suli suno supa suwi tan taso tawa telo tenpo toki tomo tu unpa uta utala walo wan waso wawa weka wile namako kin oko kipisi leko monsuta tonsi jasima kijetesantakalu soko meso epiku kokosila lanpan n misikeke ku pake apeja majuna powe'''.split():
     ww[w.replace('j','y')] = w
 ctx_ale.lists['user.nimi'] = ww
-ctx_ale.lists['user.vocabulary'] = dict(talon_vocabulary, **ww)
+#ctx_ale.lists['user.vocabulary'] = dict(talon_vocabulary, **ww)
 
 ctx = Context()
 ctx.matches = '''
